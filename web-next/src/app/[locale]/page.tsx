5df0809477fa5b05447d426ca5313b02a2809056
Crodic <alice01422@gmail.com>
2026-02-14 07:19:13 +0700

    update source base

--- web-next/src/app/[locale]/page.tsx
+++ web-next/src/app/[locale]/page.tsx
@@ -1,5 +1,3 @@
-import ArtworkCard from "@/components/features/artwork-card";
-import { PaginationWithLinks } from "@/components/features/pagination-with-links";
 import { Footer } from "@/components/layouts/footer";
 import { Header } from "@/components/layouts/header";
 import Layout from "@/components/layouts/layout";
@@ -14,38 +12,7 @@ export default async function RootPage({
   return (
     <Layout>
       <Header />
-      <div className="flex min-h-screen flex-col items-center justify-center px-8 py-24 md:px-24">
-        <section className="w-full pb-24">
-          <h1 className="text-center text-4xl font-bold">
-            Welcome to{" "}
-            <span className="text-primary dancing-script-font text-5xl">
-              Visel Art
-            </span>
-          </h1>
-        </section>
-        <section className="w-full space-y-4">
-          <div>
-            <h1 className="text-4xl font-bold">Latest Artworks</h1>
-            <p className="text-muted-foreground mt-4">
-              Check out the latest artworks
-            </p>
-          </div>
-          <div className="mx-auto grid w-full grid-cols-2 gap-4 md:grid-cols-6">
-            {Array.from({ length: 100 }).map((_, index) => (
-              <ArtworkCard
-                key={index}
-                artwork={{ name: "Hello", slug: "/1" }}
-              />
-            ))}
-          </div>
-          <PaginationWithLinks
-            page={Number(page)}
-            pageSize={20}
-            totalCount={100}
-            navigationMode="router"
-          />
-        </section>
-      </div>
+      <div className="flex min-h-screen flex-col items-center justify-center px-8 py-24 md:px-24"></div>
       <Footer />
     </Layout>
   );
--- web-next/src/app/[locale]/users/[id]/page.tsx
+++ web-next/src/app/[locale]/users/[id]/page.tsx
@@ -1,31 +1,3 @@
-import ArtworkCard from "@/components/features/artwork-card";
-import { PaginationWithLinks } from "@/components/features/pagination-with-links";
-import { FileImageIcon } from "lucide-react";
-
-export default async function UserProfile({
-  searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string | undefined }>;
-}) {
-  const page = (await searchParams).page || 1;
-
-  return (
-    <section className="w-full space-y-8">
-      <div className="flex items-center gap-2">
-        <FileImageIcon />
-        <h1 className="text-2xl font-bold">My Artworks</h1>
-      </div>
-      <div className="grid grid-cols-2 gap-2 md:grid-cols-6 md:gap-6">
-        {Array.from({ length: 100 }).map((_, index) => (
-          <ArtworkCard key={index} artwork={{}} />
-        ))}
-      </div>
-      <PaginationWithLinks
-        page={Number(page)}
-        pageSize={20}
-        totalCount={100}
-        navigationMode="link"
-      />
-    </section>
-  );
+export default async function UserProfile() {
+  return <section className="w-full space-y-8"></section>;
 }
